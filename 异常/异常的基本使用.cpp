dc7417ec4ea45bbcbc27c95829aa801a3abe2eca
Sunnyhana <1804410684@qq.com>
2021-03-11 18:52:17 +0800

    1

--- 异常/异常的基本使用.cpp
+++ 异常/异常的基本使用.cpp
@@ -5,7 +5,9 @@ int mydevide(int a, int b)
 {
     if (b == 0)
     {
-        return -1;
+        //eturn -1;
+        //throw -1;    //!抛出整型异常
+        throw 1.34; //!  抛出double异常，异常必须处理，
     }
     return a / b;
 }
@@ -14,7 +16,19 @@ void test1()
 {
     int a = 10, b = 0;
 
-    int ret = mydevide(a, b);
+    //int ret = mydevide(a, b);   //?早期返回-1，无法判断是结果还是异常
+    try
+    {
+        int ret = mydevide(a, b); /* code */
+    }
+    catch (int) //?    捕获异常
+    {
+        cout << "捕获int类型异常" << endl;
+    }
+    catch (double)
+    {
+        cout << "捕获double型异常" << endl;
+    }
 }
 
 int main()
